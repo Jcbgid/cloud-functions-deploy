11830596b11a92e8465ad0a6cf5e8d58c8899e73
Jacob Gideon <jgid@hey.com>
2022-02-21 15:06:29 -0600

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -2,13 +2,10 @@ import os
 import subprocess
 import sys
 
-from dev_deploy_def import dev_deploy_vars, dev_deploy_spec, DEV_GCLOUD_UTIL_CONFIG
-from prod_deploy_def import prod_deploy_spec, prod_deploy_vars, PROD_GCLOUD_UTIL_CONFIG
+from dev_deploy_def import deploy_vars as dev_deploy_vars, deploy_spec as dev_deploy_spec, GCLOUD_UTIL_CONFIG
 
-DEPLOY_DIRECTORY = "_temp_deploy"
-LIB_DIRECTORY = ""
-
-
+DEPLOY_DIRECTORY = "_temp_cf_deploy"
+LIB_DIRECTORY = "deps"
 
 """
 
@@ -31,11 +28,6 @@ def main():
         env_vars = dev_deploy_vars
         deploy_spec = dev_deploy_spec
         gcloud_util_config = DEV_GCLOUD_UTIL_CONFIG
-        
-    elif dep_env == "prod":
-        env_vars = prod_deploy_vars
-        deploy_spec = prod_deploy_spec
-        gcloud_util_config = PROD_GCLOUD_UTIL_CONFIG
     else:
         raise Exception
 
@@ -47,20 +39,25 @@ def main():
 
     subprocess.call("cp -a {} {}".format(LIB_DIRECTORY, DEPLOY_DIRECTORY), shell=True, stdout=subprocess.PIPE)
 
+    subprocess.call("cp -a {} {}".format("requirements.txt", DEPLOY_DIRECTORY), shell=True, stdout=subprocess.PIPE)
+
     os.chdir(os.path.dirname(os.path.realpath(__file__)) + "/{}".format(DEPLOY_DIRECTORY))
 
-    allow_unauthenticated_clause = "--allow-unauthenticated" if deploy_spec[cloud_function_name]['allow_unauthenticated'] else ""
+    allow_unauthenticated_clause = "--allow-unauthenticated" if deploy_spec[cloud_function_name][
+        'allow_unauthenticated'] else ""
 
     env_vars_clause = "--set-env-vars "
     for k, v in env_vars.items():
         env_vars_clause += "{}={},".format(k, v)
 
-    subprocess.call("gcloud config configurations activate {}".format(gcloud_util_config), shell=True, stdout=subprocess.PIPE)
+    subprocess.call("gcloud config configurations activate {}".format(gcloud_util_config), shell=True,
+                    stdout=subprocess.PIPE)
 
     print("Deploying {}... {}".format(cloud_function_name, allow_unauthenticated_clause))
     proc = subprocess.call("gcloud functions deploy {} --entry-point {} --runtime python39 --trigger-http {} {}"
-                            "".format(cloud_function_name, cloud_function_name, env_vars_clause, allow_unauthenticated_clause), shell=True,
-                            stdout=subprocess.PIPE)
+                           "".format(cloud_function_name, cloud_function_name, env_vars_clause,
+                                     allow_unauthenticated_clause), shell=True,
+                           stdout=subprocess.PIPE)
 
 
 if __name__ == "__main__":
