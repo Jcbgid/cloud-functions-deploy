060c42d6a892505ea6fe9d0fd9e8651ecff71e53
Jacob Gideon <jgid@hey.com>
2022-02-21 15:08:58 -0600

    Update run_local.py

--- run_local.py
+++ run_local.py
@@ -2,10 +2,11 @@ import os
 import signal
 import subprocess
 import sys
-from dev_deploy_def import dev_deploy_vars
+from dev_deploy_def import deploy_vars
 
 RUN_DIRECTORY = "_temp_run_local"
-LIB_DIRECTORY = ""
+LIB_DIRECTORY = "deps"
+DEF_FILE = "local_run_def.py"
 
 
 def main():
@@ -19,20 +20,17 @@ def main():
 
     subprocess.call("cp -a {} {}".format(LIB_DIRECTORY, RUN_DIRECTORY), shell=True, stdout=subprocess.PIPE)
 
+    subprocess.call("cp {} {}".format(DEF_FILE, RUN_DIRECTORY), shell=True, stdout=subprocess.PIPE)
+
     os.chdir(os.path.dirname(os.path.realpath(__file__)) + "/{}".format(RUN_DIRECTORY))
 
-    for k,v in dev_deploy_vars.items():
-        subprocess.call("export '{}={}'".format(k, v), shell=True, stdout=subprocess.PIPE)
+    proc = subprocess.Popen("functions_framework --target={} --signature-type=http".format(cloud_function_name), shell=True, stdout=subprocess.PIPE)
+    for line in proc.stdout:
+        print(line)
 
-    # This is causing problems.s
-    # proc = subprocess.Popen("functions_framework --target={} --signature-type=http".format(cloud_function_name), shell=True, stdout=subprocess.PIPE)
-    # for line in proc.stdout:
-    #     print(line)
-
-    print("functions_framework --target={} --signature-type=http".format(cloud_function_name))
 
 if __name__ == "__main__":
-    # try:
-    main()
-    # finally:
-    #     os.killpg(0, signal.SIGKILL)
+    try:
+        main()
+    finally:
+        os.killpg(0, signal.SIGKILL)
